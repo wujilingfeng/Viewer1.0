9bcf8a06c128d52ec54ffbefe7b7e58cf3f475a3
wujilingfeng <lishifucun@163.com>
2021-01-28 11:34:37 +0800

    test

--- README.md
+++ README.md
@@ -2,73 +2,60 @@
 
    可视化库，该库的目标是给一种用户可以抽象描述可视化事物而无视工程实现的工具库。比如一个数学专业的人员只需要学习c语言的成本就可以实现自己想要的的可视化任务，而无需耗费大量的成本在各种可视化工程框架中。该库目前也支持UI框架，它同样有上述的特点。
 
-   该库的框架分为抽象描述部分和工程实现部分，抽象描述部分建立在c语言上，目前工程解释器部分是基于opengl。抽象描述部分只依赖c语言，不依赖任何工程部分。
+   该库的框架分为抽象描述部分和工程实现部分，抽象描述部分建立在c语言上，目前工程解释器部分是基于opengl实现的。抽象描述部分只依赖c语言，不依赖任何工程部分。
+
+​	欢迎基于其他工程框架写的解释器，这样可以方便的将此库移植到其他平台，比如WebGL。
 
 
 
 #### Dedenpencies
 
-glfw glad freetype cstructures
+glfw：用于opengl的窗口系统。
+
+glad: opengl的实现库。
+
+freetype: 字体渲染库
+
+cstructures: c语言的数据结构库 
 
 在linux你需要先运行
 ```bash
 sudo apt install cmake xorg-dev libglu1-mesa-dev build-essential libgl1-mesa-dev
 ```
 #### Compile
-##### Compile Library
+##### compile library only
 
 glfw cstructures freetype glad文件放在thirdpart中
 
 for windows cl
 
 ```bash
-xmake f --kind=y
+xmake f --configure=y --demo=n
 xmake
 ```
 
 for windows mingw(暂时只能通过cmake来做)
 
 ```bash
-xmake f -p mingw --kind=y
-xmake
-xmake f -p mingw --kind=n
+xmake f -p mingw --configure=y --demo=n
 xmake project -k cmake
 ```
 
 for linux
 
 ```bash
-xmake f --kind=y
-xmake 
-xmake f --kind=n
+xmake f --configure=y --demo=n
 xmake
 ```
 
-##### Complier Demo
+##### complier library and demo
 
-将上面编译好的lib库放到lib文件
+将freetype glfw cstructures编译好的lib库放到lib文件
 
-for windows mingw(暂时只能通过cmake来做)
-
-```bash
-xmake f -p mingw --kind=y
-xmake
-xmake f -p mingw --kind=n
-xmake project -k cmake
-```
-for linux
-
-```bash
-xmake f --kind=y
-xmake 
-xmake f --kind=n
-xmake
-```
+针对各种平台只需将上面`--demo=n`替换为`--demo=y`
 ##### complier freetype
 
 freetype已经支持cmake
-
-
 
 #### Tutorial
 
--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,13 @@
 add_rules("mode.debug","mode.release")
-
-if has_config("kind") then
-    target("config")
+option("configure")
+    set_default(false)
+    set_showmenu(true)
+option("demo")
+    set_default(true)
+    set_showmenu(true)
+if has_config("configure") 
+then
+    target("configure1")
         on_load(function ( target )
             import("core.project.config")
             str=path.absolute(config.buildir())
@@ -9,8 +15,22 @@ if has_config("kind") then
             str2="#define MESH_VIEWER_PATH \""..str1.."/..\""
             io.writefile("include/config.h",str2)
         end)
-
+    includes("src/*.lua")
+    if has_config("demo") 
+    then
+        target("main")
+            add_deps("viewer")
+            set_targetdir("bin")
+            set_kind("binary")
+            add_files("Demo/*.cpp","Demo/*.c")
+            add_includedirs("include","Demo/include","thirdpart/cstructures/include","thirdpart/glad/include","thirdpart/glfw/include","thirdpart/freetype/include")
+            add_links("cstructures","glfw3","opengl32","freetype","viewer","gdi32","comdlg32","advapi32","Z")
+            add_linkdirs("lib","temp_libs")
+            add_ldflags("-no-pie -Wl,--start-group ") 
+    end
 end
+
+
 -- if not has_config("kind") then
 --     target("viewer_matrix4x4")
 --         set_kind("static")
@@ -18,21 +38,19 @@ end
 --         add_files("src/Math/*.c")
 --         add_includedirs("include")
 -- end
-if not has_config("kind") then
+-- if not has_config("kind") then
 
-    includes("src/*.lua")
 
-    target("main")
-        add_deps("viewer")
-        set_targetdir("bin")
-        set_kind("binary")
-        add_files("Demo/*.cpp","Demo/*.c")
-        add_includedirs("include","Demo/include","thirdpart/cstructures/include","thirdpart/glad/include","thirdpart/glfw/include","thirdpart/freetype/include")
-        add_links("cstructures","glfw3","opengl32","freetype","viewer","gdi32","comdlg32","advapi32","Z")
-        add_linkdirs("lib","temp_libs")
-        add_ldflags("-no-pie -Wl,--start-group ")
-    ---add_ldflags("-Wl,--start-group  -lglfw3 -lfreetype -lviewer -lcstructures -lZ -Wl,--end-group",{force=true}) 
-end
+--     target("main")
+--         add_deps("viewer")
+--         set_targetdir("bin")
+--         set_kind("binary")
+--         add_files("Demo/*.cpp","Demo/*.c")
+--         add_includedirs("include","Demo/include","thirdpart/cstructures/include","thirdpart/glad/include","thirdpart/glfw/include","thirdpart/freetype/include")
+--         add_links("cstructures","glfw3","opengl32","freetype","viewer","gdi32","comdlg32","advapi32","Z")
+--         add_linkdirs("lib","temp_libs")
+--         add_ldflags("-no-pie -Wl,--start-group ")
+-- end
 
 -- target("main")
 --     set_targetdir("bin")
